f737daece1881ac184b98c8379346f9c7b71ddff
Anamika Sawhney <anamika.sawhney22@gmail.com>
2025-07-18 10:09:53 +0530

    Add project files.

---
+++ OopsDemo/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+
+namespace OopsDemo
+{
+    //int num;  Int32
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            // object declaration
+            Student student; // instance of a class > object
+            student = new Student();
+            // new keyword will allocate memory
+            // // on heap
+
+            Student student1 = new Student();
+            // nwe keyword will do 2 things
+            // 1. it allocates memory on heap
+            // 2. it calls/invokes constructor to initialize member variables
+
+            //int num1, num2, num3, num4;
+            //student1.GetDetails();
+            //student1.DisplayDetails();
+            // declare 3 objects
+            //Student s1 = new Student();
+            //Student s2 = new Student(); 
+            //Student s3 = new Student();
+            //Console.WriteLine("enter details of student 1 ");
+            //s1.GetDetails();
+            //Console.WriteLine("enter details of student 2 ");
+            //s2.GetDetails();
+            //Console.WriteLine("enter details of student 3 ");
+            //s3.GetDetails();
+            //Console.WriteLine("Details of student 1");
+            //s1.DisplayDetails();
+            //Console.WriteLine("Details of student 2");
+            //s2.DisplayDetails();
+            //Console.WriteLine("Details of student 3");
+            //s3.DisplayDetails();
+            // Array of objects
+
+            //Student[] students = new Student[10];
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine("enter details of student no " + (i + 1));
+            //    students[i] = new Student();
+            //    students[i].GetDetails();
+            //}
+            //foreach(Student temp in students)
+            //{
+            //      if(temp!=null)
+            //    {
+            //        temp.DisplayDetails();
+            //    }
+            //
+            //}
+
+            // collection to store objects
+            //ArrayList students = new ArrayList();   
+            //Student s1= new Student();
+            //s1.GetDetails();
+            //students.Add(s1);
+            //Student s2 = new Student();
+            //s2.GetDetails();
+            //students.Add(s2);
+            //Student s3 = new Student();
+            //s3.GetDetails();
+            //students.Add(s3);
+            ////students.Add(1);
+            //foreach (Student s in students)
+            //{
+            //    s.DisplayDetails();
+            //}
+
+            List<Student> students = new List<Student>();
+            Student s1 = new Student();
+            s1.GetDetails();
+            students.Add(s1);
+            Student s2 = new Student();
+            s2.GetDetails();
+            students.Add(s2);
+            Student s3 = new Student();
+            s3.GetDetails();
+            students.Add(s3);
+            //students.Add(1);
+            foreach (Student s in students)
+            {
+                s.DisplayDetails();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ OopsDemo/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsDemo
+{
+    //encapsulation > binding of the members of a class
+    //abstraction > means hiding/showing details
+    // they go side by side
+    // acheived thru access specifiers
+    // 1. private  // by def all the members of a class are private, cannot access them outside the class 
+    // 2. public  // the members which we can access outside the class
+    // 3. internal // by def class is internal, can be accessesd in same assembly/project
+    // 4. protected   // used in inheritance
+    // 5. internal protected  // used in inheritance
+    // Data hiding > data shud be hidden from outside the class
+    class Student
+    {
+        int rn;
+        string name;
+        string batch;
+        int score;
+        public void GetDetails()
+        {
+            Console.WriteLine("enter rn");
+            rn = byte.Parse(Console.ReadLine());
+            Console.WriteLine("enter name");
+            name = Console.ReadLine();
+            Console.WriteLine("enter batch");
+            batch = Console.ReadLine();
+            Console.WriteLine("enter score");
+            score = byte.Parse(Console.ReadLine());
+
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"RN is {rn}");
+            Console.WriteLine($"Name is {name}");
+            Console.WriteLine($"Batch is {batch}");
+            Console.WriteLine($"Score is {score}");
+        }
+
+
+    }
+}
