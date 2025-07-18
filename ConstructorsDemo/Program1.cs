711574560836e341b9ad0e5c3f5bcd68cdbd1912
Anamika Sawhney <anamika.sawhney22@gmail.com>
2025-07-18 17:20:58 +0530

    added props/list initializer

---
+++ ConstructorsDemo/Program1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Program1
+    {
+        static void Main()
+        {
+             Student student = new Student();
+            student.Rn = 10;
+            Console.WriteLine("rn is " + student.Rn);
+            // how to initialize object using property
+            // Object initializer
+            Student student1 = new Student()
+            {
+                Rn = 10,
+                Name = "Deepak",
+                Age = 19,
+                Score = 90
+            };
+
+            // List initializer
+            List<Student> students = new List<Student>()
+            {
+                new Student (){ Rn=1, Name="a", Age=19, Score=89},
+                new Student (){ Rn=2, Name="a1", Age=19, Score=89},
+                new Student (){ Rn=3, Name="a2", Age=19, Score=89},
+                new Student (){ Rn=4, Name="a3", Age=19, Score=89},
+                new Student (){ Rn=5, Name="a4", Age=19, Score=89},
+                new Student (){ Rn=6, Name="a5", Age=19, Score=89}
+
+            };
+
+            Console.WriteLine("List of students");
+
+            foreach (Student student2 in students)
+            {
+                Console.WriteLine(student2.Rn + " " + student2.Name + " " + student2.Age + " " + student2.Score); ;
+
+            }
+
+
+
+
+
+
+
+            }
+    }
+}
---
+++ ConstructorsDemo/PropsDemo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+    namespace Demo
+    {
+        class Student
+        {
+        // property is a wrapper around pvt variable
+        // it has get & set methods
+        // get allows you to return value
+        // set allows you to put value in the pvt variable
+        // property shud be public
+        int rn;
+        public int Rn
+        {
+            get
+            {
+                 return rn;
+            }
+            set
+            {
+                if(value<0) rn = 0;
+                rn = value;
+            }
+        }
+
+        string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;   
+            }
+        }
+        //intrinsic property
+        public int Score { get;   set; }
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        //public int DayofBirth { get; set; }
+        //public string Batch { get; set; }
+
+
+    }
+    }
+
